91f6893e20ff9dd8ce1f118bbe3720df10df3cc6
Johan Axelsson <johan.axelsson@junkyard.eu>
2022-02-12 11:19:24 +0100

    sv locale

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const path = require("path");
 const withPWA = require("next-pwa");
 module.exports = withPWA({
+    i18n: {
+        locales: ["sv"],
+        defaultLocale: "sv",
+        localeDetection: false,
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
         cssModules: true,
